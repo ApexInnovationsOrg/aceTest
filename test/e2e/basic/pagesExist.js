e46888d1ba5b899e403c851873e2fe6c009b6410
JohnKlein985 <John@apexinnovations.com>
2020-03-12 16:37:28 -0500

    Ignore lock. Added to testing.
    afterEach, check for php Warning

--- test/e2e/basic/pagesExist.js
+++ test/e2e/basic/pagesExist.js
@@ -1,5 +1,8 @@
 module.exports = {
     '@tags':['basic'],
+	afterEach:function(browser){
+		browser.assert.not.containsText('html','Warning:')
+	},
     'Home Page exists': function(browser){
         browser.url('https://' + process.env.HOST)
         .waitForElementVisible('body')
