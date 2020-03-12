fd78f7ca7c129ecca0cecc4e5805028db693d8be
lustrien <50838141+lustrien@users.noreply.github.com>
2020-03-12 16:13:42 -0500

    added basic pages
    OP and Team

---
+++ test/e2e/basic/pagesExist.js
@@ -0,0 +1,33 @@
+module.exports = {
+    '@tags':['basic'],
+    'Home Page exists': function(browser){
+        browser.url('https://' + process.env.HOST)
+        .waitForElementVisible('body')
+        .assert.elementPresent('body')
+    },
+    'Store exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/store")
+        .waitForElementVisible('body')
+        .assert.elementPresent('.sunburst')
+    },
+    'Admin Site exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/store")
+        .waitForElementVisible('body')
+        .assert.elementPresent('.sunburst')
+    },
+    'Education Page exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/products.html")
+        .waitForElementVisible('body')
+        .assert.elementPresent('#productModal')
+    },
+    'Team Page exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/team.html")
+        .waitForElementVisible('body')
+        .assert.elementPresent('canvas')
+    },
+    'OP Page exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/op.html")
+        .waitForElementVisible('body')
+        .assert.elementPresent('.container .bigPadTop')
+    }
+}
--- test/e2e/basic/teamPage.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    '@tags':['basic'],
-    'Home Page exists': function(browser){
-        browser.url('https://' + process.env.HOST + "/team.html")
-        .waitForElementVisible('body')
-        .assert.elementPresent('canvas')
-    }
-}
