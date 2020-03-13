cbe041ed615eec55a93399388e9d7a7ab747f759
Eddie Muller <eddimull@yahoo.com>
2020-03-12 21:56:24 -0500

    added all the basic page checks

--- test/e2e/basic/pagesExist.js
+++ test/e2e/basic/pagesExist.js
@@ -32,5 +32,21 @@ module.exports = {
         browser.url('https://' + process.env.HOST + "/op.html")
         .waitForElementVisible('body')
         .assert.elementPresent('.container .bigPadTop')
+    },
+    'Careers Page exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/careers.html")
+        .waitForElementVisible('body')
+        .assert.containsText('body', "Teamwork makes the dream work!")
+    },
+    'Contact Us Page exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/contactUs.html")
+        .waitForElementVisible('body')
+        .assert.containsText('body', "Having trouble logging in? Course not loading?")
+    },
+    'News Page exists': function(browser){
+        browser.url('https://' + process.env.HOST + "/contactUs.html")
+        .waitForElementVisible('body')
+        .assert.elementPresent(".translucentWhiteBackground")
     }
+    
 }
