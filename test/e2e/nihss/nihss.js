a070000d7bf9f9ddc0ef694a7ad98b4ba9621569
eddimull <eddimull@gmail.com>
2020-03-31 10:44:21 -0500

    used more for making my life easier ripping the videos off of youtube.

---
+++ test/e2e/nihss/nihss.js
@@ -0,0 +1,165 @@
+module.exports = {
+    '@tags':['closedcaptioning'],
+    videoList:[{youtube:'SWlDRE-HF3M',vimeo:'401052234'}, {youtube:'VRltbAO_Fu8',vimeo:'401052274'}, {youtube:'HxzDfVjI5xY',vimeo:'401052296'}, {youtube:'BZnAaEysqYA',vimeo:'401052323'}, {youtube:'TD6xe6ursZY',vimeo:'401052339'}, {youtube:'XaohEd5-Wk0',vimeo:'401052368'}, {youtube:'DmyUceVnADs',vimeo:'401052015'}, {youtube:'DmyUceVnADs',vimeo:'401052035'}, {youtube:'C9NZSG5be_Q',vimeo:'401052065'}, {youtube:'C9NZSG5be_Q',vimeo:'401052085'}, {youtube:'MoUr3DzcK-M',vimeo:'401052109'}, {youtube:'8EFZKNOZQio',vimeo:'401052129'}, {youtube:'zhsC6Cend34',vimeo:'401052154'}, {youtube:'5XBk8pd4JlU',vimeo:'401052187'}, {youtube:'Ys_tVYrjWEQ',vimeo:'401052211'}, {youtube:'x7VqfffEZH4',vimeo:'401062476'}, {youtube:'dKhyeIf0994',vimeo:'401062492'}, {youtube:'idJ3tcekyMc',vimeo:'401062517'}, {youtube:'C-nINGqkWAM',vimeo:'401062540'}, {youtube:'c8XydozS0eE',vimeo:'401062567'}, {youtube:'Hbu5Uj1WuEA',vimeo:'401062584'}, {youtube:'_DOkyKdDJeE',vimeo:'401062627'}, {youtube:'_DOkyKdDJeE',vimeo:'401062627'}, {youtube:'E5eKJkUrLEA',vimeo:'401062665'}, {youtube:'E5eKJkUrLEA',vimeo:'401062665'}, {youtube:'A7qKF3rgMpI',vimeo:'401062712'}, {youtube:'Fzu0eXAFEX4',vimeo:'401062730'}, {youtube:'f2_Y_AI1Kqw',vimeo:'401062749'}, {youtube:'rN8gt4DReBA',vimeo:'401062423'}, {youtube:'8qzZAHT9LP4',vimeo:'401062456'}, {youtube:'S5Jb-EqkvNE',vimeo:'401068619'}, {youtube:'AONDT5KCk-Q',vimeo:'401068654'}, {youtube:'U2zwmHjC4zk',vimeo:'401068692'}, {youtube:'x99Y3K6IN0Q',vimeo:'401065218'}, {youtube:'MwrRV-4LIrI',vimeo:'401065234'}, {youtube:'5GcnKJ0gYh8',vimeo:'401065256'}, {youtube:'ez8ejrW6YWQ',vimeo:'401068872'}, {youtube:'ez8ejrW6YWQ',vimeo:'401068901'}, {youtube:'cjqAtLAt9D0',vimeo:'401068949'}, {youtube:'cjqAtLAt9D0',vimeo:'401068986'}, {youtube:'Y1C8ru1UGaw',vimeo:'401069024'}, {youtube:'W8Ijo8f8K9s',vimeo:'401069062'}, {youtube:'KBK5lF2pIdM',vimeo:'401069099'}, {youtube:'_LZ7sVXeBik',vimeo:'401069124'}, {youtube:'T944yjwT3Kg',vimeo:'401068061'}, {youtube:'pxA5iqpU1lU',vimeo:'401068099'}, {youtube:'eW8C1tlTPTY',vimeo:'401068136'}, {youtube:'qxu0Irs7sTg',vimeo:'401068165'}, {youtube:'G7LTVN_N_Qg',vimeo:'401068202'}, {youtube:'I3BT8CK_dfM',vimeo:'401068242'}, {youtube:'-gnC_jSjohg',vimeo:'401068285'}, {youtube:'BdO3I6qiTNY',vimeo:'401068336'}, {youtube:'BdO3I6qiTNY',vimeo:'401068336'}, {youtube:'Bmq1XKKTe-Q',vimeo:'401068411'}, {youtube:'Bmq1XKKTe-Q',vimeo:'401068411'}, {youtube:'6MUAM_4_pA4',vimeo:'401068474'}, {youtube:'GTxM6OMR_yc',vimeo:'401068508'}, {youtube:'d11JGwRqJ9w',vimeo:'401068550'}, {youtube:'v4giHdrZcD8',vimeo:'401068584'}, {youtube:'mZy0JSwMViA',vimeo:'401070493'}, {youtube:'5CpQ-DBqpV8',vimeo:'401070511'}, {youtube:'A_FR7ACmk1Q',vimeo:'401070535'}, {youtube:'tY-mQ1F3zNY',vimeo:'401070550'}, {youtube:'bo7bT7n7Yc8',vimeo:'401070571'}, {youtube:'fEYH11lLwFo',vimeo:'401070592'}, {youtube:'Z871upntlb8',vimeo:'401070154'}, {youtube:'7wNvn6omzBk',vimeo:'401070194'}, {youtube:'7wNvn6omzBk',vimeo:'401070194'}, {youtube:'9QS7qhwhdeU',vimeo:'401070236'}, {youtube:'9QS7qhwhdeU',vimeo:'401070236'}, {youtube:'3EOyPhE1jKU',vimeo:'401070389'}, {youtube:'QabJHu_7DzM',vimeo:'401070414'}, {youtube:'9yMmuURJt9s',vimeo:'401070435'}, {youtube:'HBGM3AxW9DE',vimeo:'401070463'}, {youtube:'Qy9lnxc_z7g',vimeo:'401070468'}, {youtube:'WHkdCy-i3N8',vimeo:'401071030'}, {youtube:'_SPFHWpNl0Q',vimeo:'401071054'}, {youtube:'-y6xvoOmJbI',vimeo:'401071077'}, {youtube:'fHU3NMjRH88',vimeo:'401071098'}, {youtube:'IXg4zrUx3bE',vimeo:'401071115'}, {youtube:'KGtJ1pHIjtU',vimeo:'401071146'}, {youtube:'ydJVpGuOn0w',vimeo:'401071186'}, {youtube:'ydJVpGuOn0w',vimeo:'401071186'}, {youtube:'1tTXqkYbbY8',vimeo:'401071223'}, {youtube:'1tTXqkYbbY8',vimeo:'401071223'}, {youtube:'9rKYd1BL8HU',vimeo:'401070939'}, {youtube:'HdhZL1UQQe0',vimeo:'401070955'}, {youtube:'heYWEzZsTzA',vimeo:'401070973'}, {youtube:'GrvVlqQ6RGE',vimeo:'401070999'}, {youtube:'OjuBbxpog20',vimeo:'401071013'}, {youtube:'HeA4h7jYqhc',vimeo:'401071522'}, {youtube:'D_iYZE9uUV4',vimeo:'401071544'}, {youtube:'kvi_MjiTdoc',vimeo:'401071566'}, {youtube:'rad0hPoUiBc',vimeo:'401071590'}, {youtube:'K4ZhZ9DJHhE',vimeo:'401071617'}, {youtube:'Wi3HmjoiSq0',vimeo:'401071637'}, {youtube:'RvTZKcRiFLQ',vimeo:'401071681'}, {youtube:'RvTZKcRiFLQ',vimeo:'401071681'}, {youtube:'bPt6ZQRbc9A',vimeo:'401071344'}, {youtube:'bPt6ZQRbc9A',vimeo:'401071344'}, {youtube:'u33PD2l3XSM',vimeo:'401071391'}, {youtube:'7psLnjnGdMI',vimeo:'401071412'}, {youtube:'D7RkCElNso4',vimeo:'401071447'}, {youtube:'k4sE493NCjQ',vimeo:'401071462'}, {youtube:'fuM9HcmVQXY',vimeo:'401071493'}, {youtube:'sjM-DJrCj0c',vimeo:'401072335'}, {youtube:'nl0jOm5fwD8',vimeo:'401072382'}, {youtube:'kbOqhAcN_VI',vimeo:'401072432'}, {youtube:'iTFh7D8H5mI',vimeo:'401072463'}, {youtube:'82-1oXNQRU4',vimeo:'401072490'}, {youtube:'Oap3pnMi5SQ',vimeo:'401072518'}, {youtube:'-GmAFp2veHY',vimeo:'401072588'}, {youtube:'-GmAFp2veHY',vimeo:'401072588'}, {youtube:'hni8JUxcLgI',vimeo:'401072668'}, {youtube:'hni8JUxcLgI',vimeo:'401072668'}, {youtube:'nl6PfPzpJLI',vimeo:'401072751'}, {youtube:'OKaLJw3PJM0',vimeo:'401072789'}, {youtube:'dZs5egrREhE',vimeo:'401072814'}, {youtube:'pOhFiOH7Px0',vimeo:'401072844'}, {youtube:'R42hBDlKhrc',vimeo:'401072903'}, {youtube:'qPBADYPrDsA',vimeo:'401072948'}, {youtube:'w3_TMDhul5c',vimeo:'401071764'}, {youtube:'q_MSPf61Jkw',vimeo:'401071799'}, {youtube:'1YpfGFY_3Dg',vimeo:'401071840'}, {youtube:'EL2Tp2ySI0o',vimeo:'401071875'}, {youtube:'k3_5Guf4cSE',vimeo:'401071916'}, {youtube:'WhMBEIb3q_I',vimeo:'401071994'}, {youtube:'WhMBEIb3q_I',vimeo:'401071994'}, {youtube:'gNVlbZS5HAc',vimeo:'401072072'}, {youtube:'gNVlbZS5HAc',vimeo:'401072072'}, {youtube:'YryVcHRw1v0',vimeo:'401072154'}, {youtube:'L9Zm8CSHLkw',vimeo:'401072196'}, {youtube:'ILtPXyWIBjk',vimeo:'401072238'}, {youtube:'erx09m0Hac4',vimeo:'401072289'}, {youtube:'TNhTlCV4vLM',vimeo:'401078597'}, {youtube:'36HSbBfYSAg',vimeo:'401078639'}, {youtube:'aMiGYpBn04c',vimeo:'401078675'}, {youtube:'SAlXs-DE4MU',vimeo:'401078715'}, {youtube:'yw-kgNEFv4I',vimeo:'401078755'}, {youtube:'SYToLIevaHg',vimeo:'401078793'}, {youtube:'_JLxo0lukMs',vimeo:'401078848'}, {youtube:'qYtuS2oN2qQ',vimeo:'401077567'}, {youtube:'qYtuS2oN2qQ',vimeo:'401077567'}, {youtube:'apNYmUDh8HA',vimeo:'401077655'}, {youtube:'apNYmUDh8HA',vimeo:'401077655'}, {youtube:'t4Yl-Ki8PyQ',vimeo:'401077735'}, {youtube:'EzPOB7n0zeA',vimeo:'401077777'}, {youtube:'5hOhpV9U1i8',vimeo:'401077835'}, {youtube:'x3g_O7b8F74',vimeo:'401077885'}, {youtube:'pNuKi8B997M',vimeo:'401077933'}, {youtube:'jmjW3Xsp9lE',vimeo:'401077970'}, {youtube:'8HeMExRIWPs',vimeo:'401078011'}, {youtube:'FPdRtrHg6DI',vimeo:'401078034'}, {youtube:'j2U3vHLQ6OQ',vimeo:'401078061'}, {youtube:'nSFMVHG5WfY',vimeo:'401078100'}, {youtube:'GKUZzzsN9co',vimeo:'401078128'}, {youtube:'7lfx3oVOpoQ',vimeo:'401078208'}, {youtube:'7lfx3oVOpoQ',vimeo:'401078208'}, {youtube:'s3BTt75_Exg',vimeo:'401074924'}, {youtube:'s3BTt75_Exg',vimeo:'401074924'}, {youtube:'cXoV8_EIk30',vimeo:'401078379'}, {youtube:'j5ZhdewIYBI',vimeo:'401078443'}, {youtube:'dpcZR80zP58',vimeo:'401078481'}, {youtube:'MyqYO_ztUqg',vimeo:'401078519'}, {youtube:'RHfSHTzooz0',vimeo:'401078557'}, {youtube:'Jp49oyGHEqY',vimeo:'401079275'}, {youtube:'w1Dbv3wBnes',vimeo:'401079297'}, {youtube:'brQvcrvrJJY',vimeo:'401079326'}, {youtube:'05H49BZgcdk',vimeo:'401079350'}, {youtube:'LesIIOYv4QA',vimeo:'401079370'}, {youtube:'nIaJkIwboYU',vimeo:'401079390'}, {youtube:'hej_aTkTlrU',vimeo:'401079444'}, {youtube:'hej_aTkTlrU',vimeo:'401079444'}, {youtube:'hB4vuuSTCfM',vimeo:'401079123'}, {youtube:'hB4vuuSTCfM',vimeo:'401079123'}, {youtube:'58U_fSwwLYg',vimeo:'401079175'}, {youtube:'8Y3lUwBNgcE',vimeo:'401079206'}, {youtube:'_hhDxfRKOJg',vimeo:'401079230'}, {youtube:'wAcF4zV-tgo',vimeo:'401079243'}, {youtube:'o7eUU-4sjXE',vimeo:'401079261'}, {youtube:'YOE5W90rAkU',vimeo:'401080127'}, {youtube:'pjSEebTxaNM',vimeo:'401080144'}, {youtube:'QKCxCP9cwWw',vimeo:'401080165'}, {youtube:'Zb2rBR3OFO8',vimeo:'401080180'}, {youtube:'plcdM-mALrc',vimeo:'401080195'}, {youtube:'Ayf6Uh7slU4',vimeo:'401080212'}, {youtube:'bush_kJ_jbk',vimeo:'401080259'}, {youtube:'bush_kJ_jbk',vimeo:'401080259'}, {youtube:'3Asa2Wx2Gj0',vimeo:'401080303'}, {youtube:'3Asa2Wx2Gj0',vimeo:'401080303'}, {youtube:'WFVYWRWufZI',vimeo:'401080338'}, {youtube:'sqRtT83smj8',vimeo:'401080360'}, {youtube:'JehcEKPfHwo',vimeo:'401080089'}, {youtube:'aMXvr5AFd0A',vimeo:'401080110'}, {youtube:'7K-PPC5Cuck',vimeo:'401080746'}, {youtube:'t1Z2fO3KK8E',vimeo:'401080775'}, {youtube:'Ay7IoVEab8M',vimeo:'401080797'}, {youtube:'yz1JphfAlwg',vimeo:'401080817'}, {youtube:'JBdrptkrgdM',vimeo:'401080833'}, {youtube:'WKpLHfxPtAE',vimeo:'401080854'}, {youtube:'VwDBHsbRxZA',vimeo:'401080876'}, {youtube:'6UYQf_Jj3Lg',vimeo:'401080900'}, {youtube:'6UYQf_Jj3Lg',vimeo:'401080900'}, {youtube:'g9fUlRTGaNg',vimeo:'401080980'}, {youtube:'g9fUlRTGaNg',vimeo:'401080980'}, {youtube:'Bo8XBcNMebw',vimeo:'401081020'}, {youtube:'E2nhEUg5_Ng',vimeo:'401081036'}, {youtube:'J6ZUgPjwFhM',vimeo:'401081054'}, {youtube:'PbAxlYl8bjI',vimeo:'401081080'}, {youtube:'SCtSfYlbdVw',vimeo:'401080729'}, {youtube:'pHmRErDFGIo',vimeo:'401081761'}, {youtube:'eRd_Z1nMKHA',vimeo:'401081790'}, {youtube:'YiodQ9JKuZ4',vimeo:'401081835'}, {youtube:'oyPPdgimKmE',vimeo:'401081872'}, {youtube:'u6U34BnYsf0',vimeo:'401081900'}, {youtube:'KDupvB7B4yg',vimeo:'401081934'}, {youtube:'jO8_-xGCRcA',vimeo:'401081979'}, {youtube:'jO8_-xGCRcA',vimeo:'401081979'}, {youtube:'CjnmFrVouyo',vimeo:'401082685'}, {youtube:'CjnmFrVouyo',vimeo:'401082685'}, {youtube:'DGVT6VLr3iI',vimeo:'401082752'}, {youtube:'u_P776nYQUk',vimeo:'401082792'}, {youtube:'BF0dBWT_q5k',vimeo:'401082826'}, {youtube:'H5fTZhD6UjA',vimeo:'401082865'}, {youtube:'Rv4ySO8TX6s',vimeo:'401082900'}, {youtube:'3KF5AODWkhw',vimeo:'401081168'}, {youtube:'wZh_uRQ72RY',vimeo:'401081195'}, {youtube:'PT_qQmUTo_Q',vimeo:'401081233'}, {youtube:'ggv4sR2HDBQ',vimeo:'401081286'}, {youtube:'KgbV2k9gR-8',vimeo:'401081316'}, {youtube:'I8Mwb1kcWSo',vimeo:'401081347'}, {youtube:'5auUL4Lxhm4',vimeo:'401081433'}, {youtube:'5auUL4Lxhm4',vimeo:'401081433'}, {youtube:'NLG8fLCDZHk',vimeo:'401081519'}, {youtube:'NLG8fLCDZHk',vimeo:'401081519'}, {youtube:'YAH0ddw-kRI',vimeo:'401081577'}, {youtube:'r87z1xfw6ec',vimeo:'401081634'}, {youtube:'ejjtR9D2NIQ',vimeo:'401081649'}, {youtube:'O_2vQS3RfPs',vimeo:'401081677'}, {youtube:'lpQ0cZPZnAo',vimeo:'401081723'}, {youtube:'EkHflFXcxZw',vimeo:'401083641'}, {youtube:'EitRijN-aT8',vimeo:'401083686'}, {youtube:'aLS9fvs2Fg8',vimeo:'401083731'}, {youtube:'gtd1Uqa4FUA',vimeo:'401083771'}, {youtube:'VMuN64SyWQ0',vimeo:'401083800'}, {youtube:'3P6vartHwC0',vimeo:'401083830'}, {youtube:'mY-b4RCMSQc',vimeo:'401083862'}, {youtube:'mY-b4RCMSQc',vimeo:'401083862'}, {youtube:'mxvf-iCCEQE',vimeo:'401083965'}, {youtube:'mxvf-iCCEQE',vimeo:'401083965'}, {youtube:'D0TAehNkoSw',vimeo:'401084049'}, {youtube:'5xm60ypLFT4',vimeo:'401084093'}, {youtube:'W96fYCN5eMM',vimeo:'401084133'}, {youtube:'WuDZRbbAb2U',vimeo:'401084179'}, {youtube:'6yYcA39Edrs',vimeo:'401084216'}, {youtube:'urTDgdeaC3Q',vimeo:'401083043'}, {youtube:'ZEu9uLOKHVg',vimeo:'401083086'}, {youtube:'MPi9JPgqiCs',vimeo:'401083129'}, {youtube:'LeY5j4nEnCs',vimeo:'401083162'}, {youtube:'1FyYbyfTiDA',vimeo:'401083204'}, {youtube:'2kN6X7ICv6E',vimeo:'401083204'}, {youtube:'s_pggYBjT2Y',vimeo:'401083311'}, {youtube:'s_pggYBjT2Y',vimeo:'401083311'}, {youtube:'_rUceqo_E5o',vimeo:'401083377'}, {youtube:'_rUceqo_E5o',vimeo:'401083377'}, {youtube:'vU5ehsmgYZA',vimeo:'401083465'}, {youtube:'lU7lWzddRnM',vimeo:'401083499'}, {youtube:'k1X6KnpkrEs',vimeo:'401083541'}, {youtube:'-EhZ-Ky-NtA',vimeo:'401083580'}, {youtube:'oXJlni6oS5g',vimeo:'401083611'}],
+	afterEach:function(browser){
+		browser.assert.not.containsText('html','Warning:')
+    },
+    before:function(browser){
+        browser.url('https://vimeo.com/log_in')
+        .setValue('input[name="email"]','developers@apexinnovations.com')
+        .setValue('input[name="password"]','vimeopassword')
+        .click('input[type="submit"]')
+    },
+    'Home Page exists': function(browser){
+        
+        this.videoList.forEach(video => {
+            var videoTitle = 'nope';
+            browser.url('https://www.youtube.com/watch?v=' + video.youtube)
+            .pause(500)
+            .execute(function(){
+                var AVOID_CONCURRENT_CAPTIONS = true;
+                window.saveVttFiles = function() {
+                var youtubeArgs = ytplayer.config.args;
+                var videoName = youtubeArgs.title;
+                var captionTracks = JSON.parse(youtubeArgs.player_response).captions
+                    .playerCaptionsTracklistRenderer.captionTracks;
+                if (!captionTracks.length) {
+                    console.warn('No captions found for ' + videoName);
+                    return;
+                }
+                var tempNode = document.createElement('div');
+                captionTracks.forEach(function(captionData) {
+                    fetch(captionData.baseUrl)
+                    .then(function(res) {
+                        return res.text();
+                    })
+                    .then(function(xmlString) {
+                        var xmlParsed = new window.DOMParser().parseFromString(
+                        xmlString,
+                        'text/xml'
+                        );
+                        var webVttContent = '';
+                        webVttContent += 'WEBVTT ' + videoName + '\n\n';
+                        webVttContent += 'NOTE generated by youtube-vtt https://github.com/benwiley4000/youtube-vtt\n';
+                        var textElements = xmlParsed.querySelector('transcript').childNodes;
+                        Array.prototype.forEach.call(textElements, function(text, index) {
+                        webVttContent += '\n' + (index + 1) + '\n';
+                        var start = Number(text.getAttribute('start'));
+                        var end = start + Number(text.getAttribute('dur'));
+                        if (AVOID_CONCURRENT_CAPTIONS && index + 1 < textElements.length) {
+                            // youtube captions overlap so this gets rid of that
+                            var nextStart = Number(
+                            textElements[index + 1].getAttribute('start')
+                            );
+                            end = Math.min(end, nextStart);
+                        }
+                        webVttContent += formatTime(start) + ' --> ' + formatTime(end) + '\n';
+                        // remove font tags from caption content
+                        var content = text.textContent.replace(/<[^>]+>/g, '');
+                        tempNode.innerHTML = content;
+                        webVttContent += tempNode.textContent + '\n';
+                        });
+                        download(
+                        videoName + '-' + captionData.languageCode + '.vtt',
+                        webVttContent
+                        );
+                    });
+                });
+
+                function pad2(number) {
+                    // thanks https://www.electrictoolbox.com/pad-number-two-digits-javascript/
+                    return (number < 10 ? '0' : '') + number;
+                }
+
+                function pad3(number) {
+                    return number >= 100 ? number : '0' + pad2(number);
+                }
+
+                // thanks https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+                function download(filename, text) {
+                    var element = document.createElement('a');
+                    element.setAttribute(
+                    'href',
+                    'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+                    );
+                    element.setAttribute('download', filename);
+                    element.style.display = 'none';
+                    document.body.appendChild(element);
+
+                    element.click();
+
+                    document.body.removeChild(element);
+                }
+
+                function formatTime(time) {
+                    var hours = 0;
+                    var minutes = 0;
+                    var seconds = 0;
+                    var milliseconds = 0;
+                    while (time >= 60 * 60) {
+                    hours++;
+                    time -= 60 * 60;
+                    }
+                    while (time >= 60) {
+                    minutes++;
+                    time -= 60;
+                    }
+                    while (time >= 1) {
+                    seconds++;
+                    time -= 1;
+                    }
+                    milliseconds = (time * 1000).toFixed(0);
+                    return (
+                    pad2(hours) +
+                    ':' +
+                    pad2(minutes) +
+                    ':' +
+                    pad2(seconds) +
+                    '.' +
+                    pad3(milliseconds)
+                    );
+                }
+                }
+
+            })
+            .pause(500)
+            .execute('saveVttFiles()')
+            .getText('.title',function(result){
+
+                videoTitle = result.value + '-en.vtt';
+                console.log(videoTitle);
+            })
+            .pause(500)
+            browser.perform(function(){
+
+                
+                // this.url('https://www.apexinnovations.com/' + videoTitle)
+                this.url('https://vimeo.com/manage/' + video.vimeo + '/distribution')
+                .pause(100)
+                .click('div[label="Subtitles"]')
+                .click('#subtitles button[format="midDark"]')
+                .pause(100)
+                .click('.Menu_Panel_wrapper select[format="neutral"]')
+                .pause(100)
+                .click('.Menu_Panel_wrapper #language option[value="en"]')
+                .pause(100)
+                .click('#type')
+                .pause(100)
+                .click('#type option[value="captions"]')
+                .pause(100)
+                .click('#type')
+                .pause(100)
+                .setValue('input[type="file"]',require('path').resolve('C:/Users/namth/Downloads' + '/' + videoTitle))
+                .pause(3000)
+                .execute(function(){$('#subtitles').find('input').trigger('click')})
+                .pause(500)
+                .execute(function(){$('button[format="primary"]').trigger('click')})
+            })
+            
+        });
+        
+        
+    },
+   
+    
+}
